2ca72f6a2b2cf4e9b424485e5fd9032f5e791e12
Egor.S <Egorsoliakov@gmail.com>
2023-04-25 15:54:11 +0400

    exercise3

---
+++ exercise3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Вадим цифру и программа говорит выходной это или нет
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int num1 = 0;
+
+
+    if(num<1)
+    {
+         Console.WriteLine("Не корректное число");
+    }
+    else if(num>7)
+    {
+         Console.WriteLine("Не корректное число");
+    }
+    else if(num>5)
+    {
+      Console.Write(" -> Yes");
+    }
+    else
+     {
+        Console.Write(" -> No");
+     }
+
