5b53e703a826b632b40b378e3199abc10b602737
Egor.S <Egorsoliakov@gmail.com>
2023-04-25 15:00:29 +0400

    exercise1

---
+++ exercise1/Program.cs
@@ -0,0 +1,25 @@
+﻿//Программа принимающая на входе трехзначное
+// число и показывающее на выходе вторую цифру
+
+Console.Write("Введите трехзначное число: ");
+int rand = int.Parse(Console.ReadLine());
+
+TwoNumber(rand);
+
+void TwoNumber(int x)
+{
+if(x<99)  Console.WriteLine("Число не трехзначное");
+if(x>1000)  Console.WriteLine("Число не трехзначное");
+else
+
+
+{
+    int ost = x % 100;
+    int del = ost / 10;
+    Console.WriteLine("Вторая цифра " +del);
+}
+}
+
+
+
+
