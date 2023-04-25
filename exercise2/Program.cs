29a22a432ae9d71b90c3bd80eb4dfaa32df14493
Egor.S <Egorsoliakov@gmail.com>
2023-04-25 15:13:22 +0400

    exercise2

---
+++ exercise2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа выводящая третью цифру числа
+
+int rand = new Random().Next(70, 120);
+Console.WriteLine("Наше число " + rand);
+if(rand > 99)
+{
+    int ost = rand % 10;
+    Console.WriteLine("Третья цифра: " + ost);
+}
+else 
+{
+    Console.WriteLine("Третьей цифры нет");
+}
