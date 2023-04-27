6c7f172851af105b8a7556b91bc15cd3e3a5f1b2
Egor.S <Egorsoliakov@gmail.com>
2023-04-27 21:15:04 +0400

    exercise2

--- exercise2/Program.cs
+++ exercise2/Program.cs
@@ -2,23 +2,33 @@
 
 int rand = new Random().Next(1, 100000);
 Console.WriteLine("Наше число " + rand);
+
+Threenumber(rand);
+
+int Threenumber(int rand)
+{
 if(rand > 99 & rand <999)
 {
    rand = rand / 10;
    Console.WriteLine("Третья цифра: " + rand);
+   
 }
  if(rand >999 & rand < 9999)
 {
     rand = rand / 10;
     Console.WriteLine("Третья цифра: " + rand%10);
+    
 }
 else if(rand > 9999)
 {
  rand = rand / 100;
     Console.WriteLine("Третья цифра: " + rand%10);
-
+    
 }
 else
 {
     Console.WriteLine("Третьей цифры нет");
+    
+}
+return rand;
 }
